5d076665c28ac1f2ee9071014355812574fb9583
Gintare <jgintare01@gmail.com>
2024-04-12 12:23:21 +0300

    new

--- tests/slotGame.spec.ts
+++ tests/slotGame.spec.ts
@@ -32,23 +32,22 @@ import { test, expect } from '@playwright/test';
 //             .toEqual('Try again!');
 //     });
 // });
-const parameter = [['You win!'], ['Try again!']];
-
-for (const name of parameter) {
-    test(`should be able to ${name[0]}`, async ({ page }) => {
-        console.log('{parameter[0] ===', parameter[0]);
-        console.log('{parameter[1] ===', parameter[1]);
-        await page.goto('https://testingmarathon.com/testing/slot_machine_game/');
-        await expect
-            .poll(
-                async () => {
-                    await page.click('#spinButton');
-                    return await page.textContent('#result');
-                },
-                {
-                    timeout: 20_000
-                }
-            )
-            .toEqual(name[0]);
-    });
-}
+test.describe('slot machine game spec', () => {
+    const parameter = ['You win!', 'Try again!'];
+    for (const testCase of parameter) {
+        test(`should be able to ${testCase}`, async ({ page }) => {
+            await page.goto('https://testingmarathon.com/testing/slot_machine_game/');
+            await expect
+                .poll(
+                    async () => {
+                        await page.click('#spinButton');
+                        return await page.textContent('#result');
+                    },
+                    {
+                        timeout: 20_000
+                    }
+                )
+                .toEqual(testCase);
+        });
+    }
+});
