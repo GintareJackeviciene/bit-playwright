01465ee023e7667a12d1549a5751dddabed0eb65
Gintare <jgintare01@gmail.com>
2024-04-16 10:31:50 +0300

    pakoreguota-sutrumpinta

--- tests/black-box.spec.ts
+++ tests/black-box.spec.ts
@@ -1,45 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('The Mysterious Black Box', () => {
-    test('testCase 0', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/BlackBox/');
-
-        await page.fill('#userInput', ' ');
-        await page.click('//button');
-        await expect(page.locator('#result')).toContainText('0');
-    });
-    test('testCase 5', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/BlackBox/');
-
-        await page.fill('#userInput', 'aaaaa');
-        await page.click('//button');
-        await expect(page.locator('#result')).toContainText('5');
-    });
-    test('testCase 10', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/BlackBox/');
-
-        await page.fill('#userInput', 'aaaaaaaaaa');
-        await page.click('//button');
-        await expect(page.locator('#result')).toContainText('10');
-    });
-    test('testCase 100', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/BlackBox/');
-
-        await page.fill(
-            '#userInput',
-            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-        );
-        await page.click('//button');
-        await expect(page.locator('#result')).toContainText('100');
-    });
-    test('testCase 1000', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/BlackBox/');
-
-        await page.fill(
-            '#userInput',
+    const parameter = [
+        ['0', 'rtghk'],
+        ['5', 'aaaaa'],
+        ['10', 'aaaaaaaaaa'],
+        ['100', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
+        [
+            '1000',
             'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-        );
-        await page.click('//button');
-        await expect(page.locator('#result')).toContainText('1000');
-    });
+        ]
+    ];
+
+    for (const testCase of parameter) {
+        test(`should return ${testCase[0]} `, async ({ page }) => {
+            await page.goto('https://testingmarathon.com/testing/BlackBox/');
+            await page.fill('#userInput', testCase[1]);
+            await page.click('//button');
+            await expect(page.locator('#result')).toContainText(testCase[0]);
+        });
+    }
 });
