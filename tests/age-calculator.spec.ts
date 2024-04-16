1f9e88fe34345ffd7391676e460c2cbc9c0ff888
Gintare <jgintare01@gmail.com>
2024-04-16 10:17:33 +0300

    pakoreguota

--- tests/age-calculator.spec.ts
+++ tests/age-calculator.spec.ts
@@ -1,55 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
-
-const names = {
-    firstName: `${faker.person.firstName()}`,
-    lastName: `${faker.person.lastName()}`
-};
 
 test.describe('Bank Account Age Calculator spec', () => {
-    test('Gimimo data iki 2000', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/Y2K_Bank_Account_Age_Calculator/');
+    const parameter = [
+        ['1995-01-01', 'Jonas Jonaitis, Your age: 29 years'],
+        ['1996-01-01', 'Jonas Jonaitis, Your age: 28 years'],
+        ['1997-01-01', 'Jonas Jonaitis, Your age: 27 years'],
+        ['1998-01-01', 'Jonas Jonaitis, Your age: 26 years'],
+        ['1999-01-01', 'Jonas Jonaitis, Your age: 25 years'],
+        ['2000-01-01', 'Jonas Jonaitis, Your age (with Y2K bug): 124 years']
+    ];
 
-        await page.fill('#firstName', names.firstName);
-        await page.fill('#lastName', names.lastName);
-        await page.fill('#dob', '1999-01-01');
-        await page.click('//button[@onclick="calculateAge()"]');
-        await expect(page.locator('#result')).toContainText(`${names.firstName} ${names.lastName}, Your age: 25 years`);
-    });
-    test('Gimimo data po 2000', async ({ page }) => {
-        await page.goto('https://testingmarathon.com/testing/Y2K_Bank_Account_Age_Calculator/');
-
-        await page.fill('#firstName', names.firstName);
-        await page.fill('#lastName', names.lastName);
-        await page.fill('#dob', '2000-01-01');
-        await page.click('//button[@onclick="calculateAge()"]');
-        await expect(page.locator('#result')).toContainText(`${names.firstName} ${names.lastName}, Your age (with Y2K bug): 124 years`);
-    });
+    for (const testCase of parameter) {
+        test(`should be able to posible to calculate correct age for ${testCase[0]} `, async ({ page }) => {
+            await page.goto('https://testingmarathon.com/testing/Y2K_Bank_Account_Age_Calculator/');
+            await page.fill('#firstName', 'Jonas');
+            await page.fill('#lastName', 'Jonaitis');
+            await page.fill('#dob', testCase[0]);
+            await page.click('//button');
+            await expect(page.locator('#result')).toContainText(testCase[1]);
+        });
+    }
 });
-
-// test.describe('Bank Account Age Calculator spec', () => {
-//     const birthDates = ['1999-01-01', '2000-01-01'];
-//     const parameter = ['Jonas Jonaitis, Your age: 25 years', 'Jonas Jonaitis, Your age (with Y2K bug): 124 years'];
-
-//     for (const testCase of parameter) {
-//         for (const dob of birthDates) {
-//             test(`should be able to ${testCase} with birth date ${dob}`, async ({ page }) => {
-//                 await page.goto('https://testingmarathon.com/testing/Y2K_Bank_Account_Age_Calculator/');
-//                 await page.fill('#firstName', 'Jonas');
-//                 await page.fill('#lastName', 'Jonaitis');
-//                 await page.fill('#dob', dob);
-//                 await expect
-//                     .poll(
-//                         async () => {
-//                             await page.click('//button[@onclick="calculateAge()"]');
-//                             return await page.textContent('#result');
-//                         },
-//                         {
-//                             timeout: 15_000
-//                         }
-//                     )
-//                     .toEqual(testCase);
-//             });
-//         }
-//     }
-// });
