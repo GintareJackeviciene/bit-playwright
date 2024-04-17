c4798ba01b16ec812b42162a1306f80b67b1c5e7
Gintare <jgintare01@gmail.com>
2024-04-17 13:18:42 +0300

    contact delete, ping pon spec

--- tests/UI-segmentation.spec.ts
+++ tests/UI-segmentation.spec.ts
@@ -2,103 +2,53 @@ import { test, expect } from '@playwright/test';
 import { Segments } from '../app-api/segments';
 import { Contact } from '../api/contact';
 import { Contacts } from '../app-api/contacts';
-import { getRandomString } from '../test.data';
+import { getRandomString, sleep } from '../test.data';
 import { ContactsDelete } from '../app-api/contactsDelete';
 import { APP_URL, COOKIE } from '../test.data';
 
 const email = `${getRandomString(20, 'qwertyuiopasdfghjklzxcvbnm')}@lokalus.lt`;
 
-const segment = {
-    name: `segment by email = ${email}`,
-    filters: {
-        type: 'group',
-        group: {
-            junction: 'or',
-            members: [
-                {
-                    type: 'group',
-                    group: {
-                        junction: 'and',
-                        members: [
-                            {
-                                type: 'group',
-                                group: {
-                                    junction: 'and',
-                                    members: [
-                                        {
-                                            type: 'rule',
-                                            rule: {
-                                                resourceType: 'contacts',
-                                                filter: {
-                                                    filterType: 'junction',
-                                                    junction: 'and',
-                                                    filters: [
-                                                        {
-                                                            filterType: 'filter',
-                                                            filterValue: {
-                                                                operator: 'eq',
-                                                                valueType: 'string_list',
-                                                                value: {
-                                                                    operator: 'any',
-                                                                    values: [email]
-                                                                },
-                                                                property: 'email'
-                                                            }
-                                                        }
-                                                    ]
-                                                }
-                                            }
-                                        }
-                                    ]
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-    }
-};
-
-let segmentID: string;
-let contactID: string;
-
 test.describe('Segment APP spec', () => {
-    test.afterAll(async ({}) => {
-        if (segmentID !== undefined) {
-            await Segments.tryToDeleteSegment(segmentID);
-        }
-        if (contactID !== undefined) {
-            await ContactsDelete.createBatchDelete([contactID]);
-        }
+    test.afterEach(async ({ page, context }) => {
+        await page.goto(`${APP_URL}/audience/segments/contact-list`);
+        await page.type('//*[@placeholder="Search contacts by email or phone number"]', email);
+        await sleep(500);
+
+        const pagePromise = context.waitForEvent('page');
+        await page.getByText('No name').click();
+
+        const newPage = await pagePromise;
+
+        await newPage.click('//*[tid="more-actions"]');
+        await newPage.click('//div[contains(text(), "Delete")]');
+        await newPage.click('//div[contains(text(), "Yes, I’m 100% sure I want to delete the selected contacts.")]');
+        await newPage.click('//div[contains(text(), "Confirm")]');
     });
 
     test('should be able to segment by contact id', async ({ page }) => {
         // const contact = await Contact.createContactByEmail(email);
         // contactID = (await contact.json()).contactID;
 
+        //sukuriam kontakta
         const context = page.context();
         await context.addCookies([{ name: 'session.omnisend', value: COOKIE, url: APP_URL }]);
         await page.goto(`${APP_URL}/audience/imports/wizard/contact`);
         await page.fill('//input[@placeholder="Enter email"]', email);
-        console.log(email);
         await page.click('//*[contains(text(), "This person gave permission to be added to the list.")]');
         await page.click('//*[contains(text(), "Add subscriber")]');
-        await expect(page.locator('//tbody')).toContainText(email);
+        await expect(page.locator('//tbody')).toContainText(email, { timeout: 15000 });
 
+        //kuriam segmenta ir tikrinam ar kontaktas ikrenta i segmenta
         await page.goto(`${APP_URL}/audience/segments/editor/`);
         await page.click('//*[contains(text(), "Add filter")]');
         await page.click('//*[contains(text(), "Email address")]');
-
         await page.click('//div[@class="select ng-invalid"]');
-
         await page.fill('//*[@placeholder="Search or enter new value"]', email);
         await page.keyboard.press('Enter');
         await page.keyboard.press('Escape');
-
-        await page.click('//*[contains(text(), "Save & show contacts"]');
+        await page.click('//*[contains(text(), "Save & show contacts")]');
         await page.fill('//input', email);
         await page.click('//div[(text()=" Save ")]');
-        await expect(page.locator('//tbody')).toContainText(email, { timeout: 30000 });
+        await expect(page.locator('//tbody')).toContainText(email, { timeout: 25000 });
     });
 });
--- tests/ping-pong.spec.ts
+++ tests/ping-pong.spec.ts
@@ -1,17 +1,28 @@
 import { test, expect } from '@playwright/test';
+import { sleep } from '../test.data';
 
-test.describe('ping-pong', () => {
-    test('should be able to wait', async ({ page }) => {
+test.describe('ping pong spec', () => {
+    test('should be able see Game Over! Your score:0,', async ({ page }) => {
         await page.goto('https://testingmarathon.com/testing/PingPong/');
-        await expect(page.locator('//body')).toContainText('Start Game');
+        page.on('dialog', async (dialog) => {
+            expect(dialog.message()).toContain('Game Over! Your score: 0');
+            await dialog.accept();
+        });
+        await page.click('#startButton');
+        await sleep(3000);
+    });
+    test('should be able see that ball is moving', async ({ page }) => {
+        await page.goto('https://testingmarathon.com/testing/PingPong/');
+        await page.click('#startButton');
 
-        // page.on('dialog', async (dialog) => {
-        //     const message = dialog.message();
-        //     expect(message).toContain("You've already liked this post!");
-        //     await dialog.accept();
-        // });
+        const A = await page.$eval('#ball', (ball) => {
+            return ball.style.left;
+        });
+        await sleep(1000);
+        const B = await page.$eval('#ball', (ball) => {
+            return ball.style.left;
+        });
 
-        // await page.click('#demo1');
-        // await page.click('#demo1');
+        expect(A).not.toEqual(B);
     });
 });
