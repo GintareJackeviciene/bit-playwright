ff965b124c90152a5ea6927c2e8acb56f711c133
Gintare <jgintare01@gmail.com>
2024-04-25 09:14:38 +0300

    random + for

--- test.data.ts
+++ test.data.ts
@@ -40,3 +40,16 @@ export function getRandomString(length: number, characters: string) {
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+export function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+}
--- tests/tic-tac-toe.spec.ts
+++ tests/tic-tac-toe.spec.ts
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { shuffle } from '../test.data';
+
+test.describe.configure({ mode: 'serial' });
+//naudok 1 freda
 
 test.describe('TicTacToe tests', () => {
+    let T = 0;
+    let O = 0;
+    let X = 0;
     test('should be able to win  X', async ({ page }) => {
         page.on('dialog', async (dialog) => {
             expect(dialog.message()).toEqual(`X wins!`);
@@ -15,7 +22,7 @@ test.describe('TicTacToe tests', () => {
     });
     test('should be able to win  0', async ({ page }) => {
         page.on('dialog', async (dialog) => {
-            expect(dialog.message()).toContain(`O wins!`);
+            expect(dialog.message()).toEqual(`O wins!`);
             await dialog.accept();
         });
         await page.goto('/testing/TicTacToe/');
@@ -28,7 +35,7 @@ test.describe('TicTacToe tests', () => {
     });
     test('should be able to be tie', async ({ page }) => {
         page.on('dialog', async (dialog) => {
-            expect(dialog.message()).toContain(`It's a tie!`);
+            expect(dialog.message()).toEqual(`It's a tie!`);
             await dialog.accept();
         });
         await page.goto('/testing/TicTacToe/');
@@ -42,4 +49,31 @@ test.describe('TicTacToe tests', () => {
         await page.click('//*[@id="8"]');
         await page.click('//*[@id="9"]');
     });
+    for (let i = 0; i < 5; i++) {
+        test(`random game number= ${i}`, async ({ page }) => {
+            await page.goto('/testing/TicTacToe/');
+            let gameOver = false;
+
+            page.on('dialog', async (dialog) => {
+                expect(dialog.message()).toMatch(/It's a tie!|O wins!|X wins!/);
+                if (dialog.message() == `It's a tie!`) T++;
+                if (dialog.message() == 'O wins!') O++;
+                if (dialog.message() == 'X wins!') X++;
+
+                console.log('T ===', T);
+                console.log('O ===', O);
+                console.log('X ===', X);
+                gameOver = true;
+                await dialog.accept();
+            });
+
+            const positions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            shuffle(positions);
+
+            for (const position of positions) {
+                if (gameOver) break;
+                await page.click(`//*[@id="${position}"]`);
+            }
+        });
+    }
 });
